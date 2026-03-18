945a80d5f35ae59b0aff628945df6002ed6bd22f
Jason-817 <jasonnovio@hotmail.ca>
2026-03-18 02:53:13 -0400

    change txt name

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function downloadTxtFile(bioData, questionnaireData) {
     const blob = new Blob([content], {type:"text/plain"});
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = "gf_application.txt";
+    link.download = "application.txt";
     link.click();
 }
 
